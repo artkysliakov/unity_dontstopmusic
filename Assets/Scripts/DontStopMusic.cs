2b2ca7c77fa5c6b7135a46a639090506482e0175
artem88 <artemik2010@gmail.com>
2018-04-09 09:11:56 +0300

    Added assets and transitions between scenes Created C# script "DontStopMusic"

---
+++ Assets/Scripts/DontStopMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontStopMusic : MonoBehaviour
+{
+
+    private static DontStopMusic instance = null;
+    public static DontStopMusic Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
